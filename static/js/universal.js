c20616454b0d94c85066c4bbec62e710bc558ab8
MArianA Orti <ortizmariana@uninorte.edu.co>
2026-05-11 13:37:33 +0000

    cambio en univesal

--- static/js/universal.js
+++ static/js/universal.js
@@ -1,4 +1,4 @@
 var map = L.map('map').fitWorld();
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-Atribución: '© <a href="https://www.openstreetmap.org/copyright">Colaboradores de OpenStreetMap</a>'
-}).addTo(mapa);
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
