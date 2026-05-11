f2d3a3f024a2cbdd89135c1dcfb29739884186f8
Nataly Lopez <napologo1010@gmail.com>
2026-05-11 13:16:22 +0000

    Mapa

---
+++ static/css/universal.js
@@ -0,0 +1,4 @@
+var map = L.map('map').fitWorld();
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+Atribución: '© <a href="https://www.openstreetmap.org/copyright">Colaboradores de OpenStreetMap</a>'
+}).addTo(mapa);
