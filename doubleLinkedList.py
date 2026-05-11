3fafeee3f4a9da96d1a82713f83d24144c54f1d2
Nataly Lopez <napologo1010@gmail.com>
2026-05-11 13:57:20 +0000

    m

--- doubleLinkedList.py
+++ doubleLinkedList.py
@@ -4,4 +4,16 @@ class DoubleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-    
+
+    def add_child(self, parent:Node, child:Node):
+        if parent.sub_list is None:
+            sublist = LinkedList()
+            sublist.head = child
+            sublist.tail = child
+            parent.sub_list = sublist
+        else:
+            current = parent.sub_list.tail
+            current.next = child
+            child.prev = current
+            parent.sub_list.tail = child
+        return parent.sub_list
